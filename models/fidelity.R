490ceaa3f3e1f1288389321879c3281008835f9b
Pierce Edmiston <pierce.edmiston@gmail.com>
2015-09-18 08:42:16 -0500

    Integrating new audio

--- models/fidelity.R
+++ models/fidelity.R
@@ -9,3 +9,7 @@ responses <- get_responses()
 
 fidelity_mod <- glm(is_correct ~ generation * contrast_c, family = binomial, data = responses)
 summary(fidelity_mod)
+
+fidelity_mod <- glmer(is_correct ~ generation * contrast_c + (1|given),
+                      data = responses, family = binomial)
+summary(fidelity_mod)
